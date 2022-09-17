cc2af7f1bd7e802357515f1cc8c61e93b2ae5170
Apoorv Aron <83576954+apoorvaron@users.noreply.github.com>
2022-09-17 14:25:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.post("/", function (req, res) {
 
   res.redirect("/");
 });
-app.listen(process.env.PRT || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000.");
 });
